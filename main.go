32110f2c01ea9d29aa43ad5a0e520b889f9c2b6b
Misa Stefanovic <40996107+MikMikYT@users.noreply.github.com>
2023-02-12 22:35:24 +0100

    add retrieval of version json from this repo

--- main.go
+++ main.go
@@ -4,12 +4,36 @@ package main
 import "C"
 
 import (
+	"io/ioutil"
+	"net/http"
+	"os"
+
 	"github.com/nanitefactory/winmb"
 )
 
+const versionFileURL = "https://raw.githubusercontent.com/MikMik1011/gtasrbija-updatechecker/master/info/version.json"
+
+func fetchVersion() string {
+	res, err := http.Get(versionFileURL)
+	if err != nil {
+		winmb.MessageBoxPlain("GTA Srbija Update Checker", "Greska prilikom dobijanja informacije o najnovijoj verziji!")
+		os.Exit(1)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		winmb.MessageBoxPlain("GTA Srbija Update Checker", "Greska prilikom parsovanja informacije o najnovijoj verziji!")
+		os.Exit(1)
+	}
+
+	return string(body)
+
+}
+
 //export MainThread
 func MainThread() {
-	winmb.MessageBoxPlain("GTA Srbija Update Checker", "Proba proba 1312")
+	winmb.MessageBoxPlain("GTA Srbija Update Checker", fetchVersion())
+
 }
 
 func main() {
